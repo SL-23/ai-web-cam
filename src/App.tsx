4e46e95ce173ee4f1de5a49ed89175c9958ebd31
SL-23 <stephanieluvsphy@gmail.com>
2024-10-29 00:24:35 +1030

    Set up docs chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import WebCam from "./components/WebCam";
+import DocsChat from "./components/DocsChat";
 
 const App = () => {
+  const h = import.meta.env.VITE_API_URL;
+  console.log({ h });
   return (
     <>
       <WebCam />
+      <DocsChat />
     </>
   );
 };
---
+++ src/components/DocsChat/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DocsChat = () => {
+  return <div>HHH</div>;
+};
+
+export default DocsChat;
